4db26b61284ab724efe29d41f8653424911ae106
solomonsh <s.seshiferaw@gmail.com>
2024-11-13 17:50:01 +0100

    adding freinds list append

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def read_friends_file():
     with open("friends.txt", "r") as file:
         for line in file:
             line_parts = line.strip().split(" follows ")
-            
+            friends_list.append((line_parts[0], line_parts[1]))
     return friends_list
 
 print(read_friends_file())
